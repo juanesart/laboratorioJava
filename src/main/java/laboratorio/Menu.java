ee657100649d5950bfabe40a8932a540e1733ffc
juanesart <juanesartq@gmail.com>
2021-09-09 19:41:06 -0500

    modificación menu y solución punto 2

--- src/main/java/laboratorio/Menu.java
+++ src/main/java/laboratorio/Menu.java
@@ -1,11 +1,30 @@
 package laboratorio;
 import laboratorio.punto1.CompararNumeros;
+import laboratorio.punto2.CompararNumerosUser;
 
 import javax.swing.*;
 
 public class Menu {
     public static void main(String [] args){
-        System.out.println(CompararNumeros.comparar());
-        JOptionPane.showMessageDialog(null, CompararNumeros.comparar());
+        int option = optionMenu();
+
+        switch (option){
+            case 1:
+                System.out.println(CompararNumeros.comparar());
+                JOptionPane.showMessageDialog(null, CompararNumeros.comparar());
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, CompararNumerosUser.compararVariables());
+        }
+
+
+    }
+    public static int optionMenu(){
+        int opt;
+
+        opt = Integer.parseInt(JOptionPane.showInputDialog("Selecciona el ejercicio a revisar\n"+
+                "1. Punto Uno\n"+
+                "2. Punto Dos"));
+        return opt;
     }
 }
--- src/main/java/laboratorio/punto2/CompararNumerosUser.java
+++ src/main/java/laboratorio/punto2/CompararNumerosUser.java
@@ -1,5 +1,39 @@
 package laboratorio.punto2;
 
-public class CompararNumerosUser {
+import javax.swing.*;
 
+public class CompararNumerosUser {
+    private static int a;
+    private static int b;
+
+    public static String compararVariables() {
+        String respuesta = "";
+
+        while (true) {
+            try {
+                a = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero 1"));
+                System.out.println(a);
+                break;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Ingresa un numero valido, error: " + e.getMessage());
+            }
+        }
+        while (true) {
+            try {
+                b = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero 2"));
+                System.out.println(b);
+                break;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Ingresa un numero valido, error: " + e.getMessage());
+            }
+        }
+        if (a > b) {
+            respuesta = "el numero 1 es mayor";
+        } else if (a < b) {
+            respuesta = "el numero 2 es mayor";
+        } else {
+            respuesta = "ambos numeros son iguales";
+        }
+        return respuesta;
+    }
 }
